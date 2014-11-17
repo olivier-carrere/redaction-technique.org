1a8a3035c5667a4ef72e2d9edc8d4366cdb90068
Olivier Carrere <carrereo@gmail.com>
2014-11-17 07:59:28 +0100

    Script to remove extra blank lines: add support for .dita files
    Edit script to support .dita files.
    
    Signed-off-by: Olivier Carrère <carrereo@gmail.com>
    Acked-by: Angela Cristina <acristina@redaction-technique.org>

--- scripts/remove-blank-lines.sh
+++ scripts/remove-blank-lines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Delete repeated empty lines from .rst files. Only edit files containing
+# Delete repeated empty lines from .dita files. Only edit files containing
 # repeated empty lines to prevent make from building the whole project each
 # time this script is run.
 
@@ -7,8 +7,8 @@ sha1 () {
     echo $(sha1sum $1 | awk '{print $1}')
 }
 
-for file in *.rst; do
-    EDITED=/tmp/$(basename $file .rst).tmp
+for file in *.dita; do
+    EDITED=/tmp/$(basename $file .dita).tmp
     cat -s $file > $EDITED
     ORIG_SHA1=$(sha1 $file)
     EDITED_SHA1=$(sha1 $EDITED)
