8ba91272f1a9c12f56e62f8a075e7e28162da80d
Olivier Carrere <carrereo@gmail.com>
2014-12-11 15:53:04 +0100

    PDF output: set paper size to A4
    Set paper size to A4 for latexpdf output.
    
    Signed-off-by: Olivier Carrère <carrereo@gmail.com>
    Acked-by: Angela Cristina <acristina@redaction-technique.org>

--- conf.py
+++ conf.py
@@ -223,7 +223,7 @@ htmlhelp_basename = 'redaction-techniqueorgdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
