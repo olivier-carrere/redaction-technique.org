a2ad2c685bec89149e98c9c93e0af896c2d8ea13
Olivier Carrère <carrereo@gmail.com>
2017-06-19 11:09:43 +0200

    HTML: Apply sphinx_rtd_theme
    Apply sphinx_rtd_theme with custom CSS to override default menu colors.
    layout.html. Override previous Jinja template comprising Google Analytics tag
    and various external links.
    
    Signed-off-by: Olivier Carrère <carrereo@gmail.com>
    Acked-by: Angela Cristina <acristina@redaction-technique.org>

--- conf.py
+++ conf.py
@@ -157,14 +157,15 @@ rst_epilog = u"""
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "headerbg" : "grey"
-}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
