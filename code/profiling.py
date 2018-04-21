b8fadab9f4c9d910b86444836f4e0b691aeee9c9
olivier <carrereo@gmail.com>
2018-04-21 09:33:02 +0200

    profiling.py : conformité à PEP 8
    Mise en conformité avec la norme de codage PEP 8
    https://www.python.org/dev/peps/pep-0008/ via py.test --pep8.

--- code/profiling.py
+++ code/profiling.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python
 # coding: utf8
-class Audience:
-    def __init__(self,pers,seas):
-        self.personae=pers
-        self.season=seas
-        
 import jinja2
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
+class Audience:
+    def __init__(self, pers, seas):
+        self.personae = pers
+        self.season = seas
+
+public = ('electrician', 'plumber')
+four_seasons = ('winter', 'spring', 'summer', 'autumn')
 if len(sys.argv) == 3:
-    pubparam=str(sys.argv[1])
-    seasparam=str(sys.argv[2])
-    if pubparam in ('electrician','plumber') and seasparam in ('winter','spring', 'summer', 'autumn'):
-        user=Audience(pubparam,seasparam)
+    pubparam = str(sys.argv[1])
+    seasparam = str(sys.argv[2])
+    if pubparam in public and seasparam in four_seasons:
+        user = Audience(pubparam, seasparam)
         env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
         template = env.get_template('texte-conditionnel.rst')
-        string=template.render(public=user)
+        string = template.render(public=user)
         print(string)
     else:
-	print('Valeurs admises :\nelectrician ou plumber en 1er paramètre\nwinter,spring, summer, autumn en 2nd paramètre')
+	print('''Valeurs admises :\nelectrician ou plumber en 1er paramètre\nwinter,
+        spring, summer, autumn en 2nd paramètre''')
 else:
     print('Veuillez indiquer le public et la saison')
