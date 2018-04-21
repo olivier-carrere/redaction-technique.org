89845264eca5bf26064191eb09f5c0112338f659
olivier <carrereo@gmail.com>
2018-04-21 09:42:56 +0200

    word_switch.py : conformité à PEP 8 et correction
    Modification du code pour :
    
    - faciliter l’utilisation (précision sur le fait qu’il faut entrer les
      guillemets lors de la saisie) ;
    
    - afficher le résultat sur une seule ligne.
    
    Mise en conformité avec la norme de codage PEP 8
    https://www.python.org/dev/peps/pep-0008/ via py.test --pep8.

--- code/word_switch.py
+++ code/word_switch.py
@@ -1,43 +1,42 @@
 #! /usr/bin/python
+# coding: utf8
 
-"""
+'''
 
-   Demande à l'utilisateur de saisir la tirade de M. Jourdain et en affiche
-   une variante. Fonctionne uniquement avec la tirade suivante :
+   Demande à l'utilisateur de saisir la tirade de M. Jourdain et en
+   affiche une variante. Fonctionne uniquement avec la tirade
+   suivante ou comportant le même nombre de mots, entrée entre guillemets :
 
-   Belle marquise, vos beaux yeux me font mourir d'amour.
+   "Belle marquise, vos beaux yeux me font mourir d'amour."
 
-"""
+'''
 
 # Importation de la bibliothèque d'expressions régulières.
 import re
 
 # Le programme demande à l'utilisateur de saisir la tirade et la place dans
 # une variable.
-texte_original = input("Entrez la tirade de M. Jourdain : ")
+texte_original = input('Entrez la tirade de M. Jourdain entre guillemets : ')
 # Suppression du point final de la tirade.
 texte_original_sans_point = texte_original.strip('.')
 # Conversion de la chaîne de caractères en liste de mots.
-texte_melange = re.split(' ',texte_original_sans_point)
+texte_melange = re.split(' ', texte_original_sans_point)
 # Création d'une variable globale contenant une chaîne de caractères vide.
-texte_final = ""
+texte_final = ''
 
 # Après vérification que la tirade comporte bien 9 mots, affichage du premier
 # mot de la tirade modifiée, avec une lettre capitale.
 if len(texte_melange) == 9:
-   print(str.capitalize(texte_melange[8]),end="")
+    # Une boucle ajoute à la chaîne de caractères initialement vide les mots de
+    # la tirade de M. Jourdain, à l'exception du premier et du dernier, dans
+    # l'ordre voulu.
 
-   # Une boucle ajoute à la chaîne de caractères initialement vide les mots de
-   # la tirade de M. Jourdain, à l'exception du premier et du dernier, dans
-   # l'ordre voulu.
+    for i in [7, 5, 6, 0, 1, 2, 3]:
+        texte_final = texte_final + ' ' + str.lower(texte_melange[i])
 
-   for i in [7,5,6,0,1,2,3]:
-      texte_final = texte_final + " " + str.lower(texte_melange[i])
-
-   # Affichage du texte final, suivi d'un point.
-   print(texte_final,end=" ")
-   print(str.lower(texte_melange[4]),end="")
-   print(".")
+    # Affichage du texte final, suivi d'un point.
+    print(str.capitalize(texte_melange[8]) + texte_final + ' ' +
+          str.lower(texte_melange[4]) + '.')
 
 else:
-   print("La tirade doit comporter neuf mots exactement.")   
+    print('La tirade doit comporter neuf mots exactement.')
