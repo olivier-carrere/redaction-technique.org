a53b410674be17a4c0b5955c332fa87d65c2bdc3
olivier <carrereo@gmail.com>
2025-05-08 13:36:08 +0200

    astro

--- red-ring/astro.config.mjs
+++ red-ring/astro.config.mjs
@@ -14,6 +14,13 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example aGuide', slug: 'guides/diminuer-les-couts-ameliorer-la-satisfaction-client' },
+						{ label: 'Example Gcuide', slug: 'guides/redaction-technique-un-processus-industriel' },
+						{ label: 'Example Guide', slug: 'guides/format-structure-dita-xml' },
+						{ label: 'Example Guide', slug: 'guides/mener-un-projet-de-bout-en-bout' },
+						{ label: 'Example Guide', slug: 'guides/le-coin-du-geek' },
+						{ label: 'Example Guide', slug: 'guides/a-propos-de-ce-blog' },
+						{ label: 'Example Guidsse', slug: 'guides/contact' },
 					],
 				},
 				{
