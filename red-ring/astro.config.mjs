6de9b97331e8a08ab45cc47d349289c4c98d7281
olivier <carrereo@gmail.com>
2025-05-09 09:46:36 +0200

    astro

--- red-ring/astro.config.mjs
+++ red-ring/astro.config.mjs
@@ -10,19 +10,19 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
+					label: 'Tech writing',
+					items: [
+						{ slug: 'guides/diminuer-les-couts-ameliorer-la-satisfaction-client' },					
+						{ slug: 'guides/de-la-redaction-a-la-communication-technique' },
+						{ slug: 'guides/les-trois-niveaux-de-la-documentation-technique' },
+						{ slug: 'guides/principe-de-simplicite-kiss' },
+						{ slug: 'guides/formats-et-outils' },
+					],
+				},
+			
+				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ slug: 'guides/diminuer-les-couts-ameliorer-la-satisfaction-client' },
-						      {
-						              label: 'Documentation technique',
-							              items: [
-								                { slug: 'guides/de-la-redaction-a-la-communication-technique' },
-								                { slug: 'guides/les-trois-niveaux-de-la-documentation-technique' },
-								                { slug: 'guides/principe-de-simplicite-kiss' },
-								                { slug: 'guides/formats-et-outils' },
-										        ]
-											      },
 						{ slug: 'guides/format-structure-dita-xml' },
 						{ slug: 'guides/mener-un-projet-de-bout-en-bout' },
 						{ slug: 'guides/le-coin-du-geek' },
