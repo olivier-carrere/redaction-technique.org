b0195703828824089684e3fe274312c52ed2495f
olivier <carrereo@gmail.com>
2025-05-16 19:29:24 +0200

    Implement feedback widget

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,52 +1,69 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Technical Writing',
-			defaultLocale: 'en',
-			locales: {
-				fr: {
-					label: 'Français',
-					lang: 'fr',
-					title: 'Rédaction technique',
-					sidebar: [
-						{
-							label: 'À propos',
-							items: [
-								{ slug: 'fr/about-this-blog' },
-								{ slug: 'fr/migrate-to-astro' },
-
-							],
-						},
-					],
-				},
-				en: {
-					label: 'English',
-					lang: 'en',
-					title: 'Technical Writing',
-					sidebar: [
-						{
-							label: 'About',
-							items: [
-								{ slug: 'en/about-this-blog' },
-								{ slug: 'en/migrate-to-astro' },
-								{ slug: 'en/assess-documentation-quality' },
-							],
-						},
-					],
-				},
-			},
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/olivier-carrere/redaction-technique.org',
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Technical Writing',
+      defaultLocale: 'en',
+      locales: {
+        fr: {
+          label: 'Français',
+          lang: 'fr',
+          title: 'Rédaction technique',
+          sidebar: [
+            {
+              label: 'À propos',
+              items: [
+                { slug: 'fr/about-this-blog' },
+                { slug: 'fr/migrate-to-astro' },
+              ],
+            },
+          ],
+        },
+        en: {
+          label: 'English',
+          lang: 'en',
+          title: 'Technical Writing',
+          sidebar: [
+            {
+              label: 'About',
+              items: [
+                { slug: 'en/about-this-blog' },
+                { slug: 'en/migrate-to-astro' },
+                { slug: 'en/assess-documentation-quality' },
+              ],
+            },
+          ],
+        },
+      },
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/olivier-carrere/redaction-technique.org',
+        },
+      ],
+      components: {
+        Footer: './src/components/CustomFooter.astro',
+      },
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'stylesheet',
+            href: 'https://cdn.jsdelivr.net/npm/pushfeedback/dist/pushfeedback/pushfeedback.css',
+          },
+        },
+        {
+          tag: 'script',
+          attrs: {
+            type: 'module',
+            src: 'https://cdn.jsdelivr.net/npm/pushfeedback/dist/pushfeedback/pushfeedback.esm.js',
+          },
+        },
+      ],
+    }),
+  ],
 });
