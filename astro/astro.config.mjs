6887ad8166c0f8f1ba7b45d9839e8f063a204813
ZenDeshimarhu <zen.deshimaru.com@gmail.com>
2025-06-07 17:41:50 +0200

    Update astro.config.mjs

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://zendeshimarhu.github.io',
+  site: 'https://zendeshimarhu.github.io/foo',
   base: 'foo',
   integrations: [
     starlight({
